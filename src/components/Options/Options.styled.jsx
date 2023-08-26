4bcec7c1b7af951cbdfcb93b3d6941fa80d3cd72
PrimeGlorious <TheComboPunk@gmail.com>
2023-08-26 22:40:29 +0300

    add styled options

--- src/components/Options/Options.styled.jsx
+++ src/components/Options/Options.styled.jsx
@@ -6,8 +6,38 @@ export const OptionsList = styled.ul`
 `
 
 export const Button = styled.button`
-  width: 200px;
+  appearance: none;
+  background-color: transparent;
+  border: 2px solid #1A1A1A;
+  border-radius: 15px;
+  box-sizing: border-box;
+  color: #3B3B3B;
+  display: inline-block;
+  font-family: Roobert,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol";
+  font-size: 16px;
+  font-weight: 600;
+  line-height: normal;
+  margin: 0;
   height: 50px;
-  background-color: #ffffff;
-  border: 1px solid #000000;
+  width: 200px;
+  outline: none;
+
+  text-decoration: none;
+  transition: all 300ms cubic-bezier(.23, 1, 0.32, 1);
+  touch-action: manipulation;
+  will-change: transform;
+
+  &:hover {
+  color: #fff;
+  background-color: #1A1A1A;
+  box-shadow: rgba(0, 0, 0, 0.25) 0 8px 15px;
+  transform: translateY(-2px);
+
+  &:active {
+  box-shadow: none;
+  transform: translateY(0);
+  background-color: transparent;
+  color: #1A1A1A;
+}
+}
 `
