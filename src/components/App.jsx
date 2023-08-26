0e93999f5b6c224ba91397ce5aafa3ecddf099ab
PrimeGlorious <TheComboPunk@gmail.com>
2023-08-26 21:13:14 +0300

    add local storage system

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Counter } from './Counter/Counter';
 import { Options } from './Options/Options';
 
 export const App = () => {
-  const [value, setValue] = useState(0);
+  const [value, setValue] = useState(JSON.parse(window.localStorage.getItem('Value')) ?? 0);
+
+  useEffect(() => {
+    JSON.stringify(window.localStorage.setItem('Value', value));
+  }, [value]);
 
   const increase = () => {
     setValue((prevState) => prevState + 1)
