50568cd6fd42caabce020a057486c7a533a63bde
PrimeGlorious <TheComboPunk@gmail.com>
2023-08-27 14:58:38 +0300

    add buttons select

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,27 +3,93 @@ import { Counter } from './Counter/Counter';
 import { Options } from './Options/Options';
 
 export const App = () => {
-  const [value, setValue] = useState(JSON.parse(window.localStorage.getItem('Value')) ?? 0);
+  const [value1, setValue1] = useState(JSON.parse(window.localStorage.getItem('Value1')) ?? 0);
+  const [value2, setValue2] = useState(JSON.parse(window.localStorage.getItem('Value2')) ?? 0);
+  const [value3, setValue3] = useState(JSON.parse(window.localStorage.getItem('Value3')) ?? 0);
+  const [btn1, setbtn1] = useState(false);
+  const [btn2, setbtn2] = useState(false);
+  const [btn3, setbtn3] = useState(false);
+
+
 
   useEffect(() => {
-    JSON.stringify(window.localStorage.setItem('Value', value));
-  }, [value]);
+    JSON.stringify(window.localStorage.setItem('Value1', value1));
+  }, [value1]);
+
+  useEffect(() => {
+    JSON.stringify(window.localStorage.setItem('Value2', value2));
+  }, [value2]);
+
+  useEffect(() => {
+    JSON.stringify(window.localStorage.setItem('Value3', value3));
+  }, [value3]);
+
+  const onSelectBtnClick = (e) => {
+    switch (e.currentTarget.dataset.value) {
+      case '1':
+        setbtn1(true)
+        setbtn2(false)
+        setbtn3(false)
+        break;
+      case '2':
+        setbtn1(false)
+        setbtn2(true)
+        setbtn3(false)
+        break;
+      case '3':
+        setbtn1(false)
+        setbtn2(false)
+        setbtn3(true)
+        break;
+      default:
+        break;
+    }
+    return e.target.dataset.value;
+  }
 
   const increase = () => {
-    setValue((prevState) => prevState + 1)
+    if (btn1) {
+      setValue1(s => s + 1)
+      return
+    }
+
+    if (btn2) {
+      setValue2(s => s + 1)
+      return
+    }
+
+    if (btn3) {
+      setValue3(s => s + 1)
+      return
+    }
   }
 
   const decrease = () => {
-    setValue((prevState) => prevState - 1)
+    if (btn1) {
+      setValue1(s => s - 1)
+      return
+    }
+
+    if (btn2) {
+      setValue2(s => s - 1)
+      return
+    }
+
+    if (btn3) {
+      setValue3(s => s - 1)
+      return
+    }
+
+    return
   }
 
-  const reset = () => {
-    setValue(0)
+  const reset = (valueIndex) => {
+    `setValue${valueIndex}`(0)
   }
 
   return (
     <div>
-      <Counter value={value} />
+      <Counter value1={value1} value2={value2} value3={value3} onSelectBtn={onSelectBtnClick} btn1Disabled={btn1} btn2Disabled={btn2} btn3Disabled={btn3} />
       <Options increase={increase} decrease={decrease} reset={reset} />
     </div>
   );
--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,8 +1,19 @@
-function Counter({value}) {
+function Counter({value1, value2, value3, onSelectBtn, btn1Disabled, btn2Disabled, btn3Disabled}) {
   return (
-    <h1>
-      {value}
-    </h1>
+    <div>
+      <h1>
+        {value1}
+        <button onClick={onSelectBtn} data-value='1' disabled={btn1Disabled}><b>Select</b></button>
+      </h1>
+      <h1>
+        {value2}
+        <button onClick={onSelectBtn} data-value='2' disabled={btn2Disabled}><b>Select</b></button>
+      </h1>
+      <h1>
+        {value3}
+        <button onClick={onSelectBtn} data-value='3' disabled={btn3Disabled}><b>Select</b></button>
+      </h1>
+    </div>
   )
 };
 
